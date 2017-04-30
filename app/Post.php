e2e72760e3d7926e3879fb045ccccbf6f43ca092
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2017-04-30 14:42:31 +0200

    Connection belongsToMany between model Post and model Tag

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+
+    public $fillable = ['name', 'content'];
+
+    public function tags() {
+        return $this->belongsToMany(Tag::class);
+    }
+
 }
--- app/Tag.php
+++ app/Tag.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    //
+
+    public $guarded = [];
+    public $timestamps = false;
+
+    public function posts() {
+        return $this->belongsToMany(Tag::class);
+    }
+
 }
