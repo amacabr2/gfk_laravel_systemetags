de1fa5d69ba04477e540738635cf27f37def3381
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2017-04-30 14:32:29 +0200

    Create table of connection between table posts and table tags

--- database/migrations/2017_04_30_122142_create_tags_table.php
+++ database/migrations/2017_04_30_122142_create_tags_table.php
@@ -16,7 +16,7 @@ class CreateTagsTable extends Migration
         Schema::create('tags', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->slug('slug');
+            $table->string('slug');
         });
 
         Schema::create('post_tag', function (Blueprint $table) {
