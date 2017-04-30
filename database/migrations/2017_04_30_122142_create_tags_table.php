fa42301ec9bfe14ed83d20c97be59a77767a2aba
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2017-04-30 14:29:21 +0200

    Create table of connection between table posts and table tags

--- database/migrations/2017_04_30_122142_create_tags_table.php
+++ database/migrations/2017_04_30_122142_create_tags_table.php
@@ -17,7 +17,13 @@ class CreateTagsTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->slug('slug');
-            $table->timestamps();
+        });
+
+        Schema::create('post_tag', function (Blueprint $table) {
+           $table->integer('post_id')->unsigned()->index();
+           $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+           $table->integer('tag_id')->unsigned()->index();
+           $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
         });
     }
 
@@ -28,6 +34,7 @@ class CreateTagsTable extends Migration
      */
     public function down()
     {
+        Schema::dropIfExists('post_tag');
         Schema::dropIfExists('tags');
     }
 }
