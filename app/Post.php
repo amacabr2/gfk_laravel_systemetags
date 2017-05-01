24777996c6877788528ceaa74a627bd1684885be
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2017-05-01 11:36:19 +0200

    Tags not recreated if already existing in bdd when created an article with a tag who existing + test

--- app/Post.php
+++ app/Post.php
@@ -24,9 +24,11 @@ class Post extends Model
      */
     public function saveTags(String $tags) {
         $tags = explode(',', $tags);
+        $persistedTags = Tag::whereIn('name', $tags)->get();
+        $tagsToCreate = array_diff($tags, $persistedTags->pluck('name')->all());
         $tagsToCreate = array_map(function ($tag) {
             return ['name' => $tag, 'slug' => Str::slug($tag)];
-        }, $tags);
+        }, $tagsToCreate);
         Tag::insert($tagsToCreate);
     }
 
--- tests/Feature/TaggableTest.php
+++ tests/Feature/TaggableTest.php
@@ -4,28 +4,38 @@ namespace Tests\Feature;
 
 use App\Post;
 use App\Tag;
+use Faker\Factory;
 use Illuminate\Support\Facades\Artisan;
 use Tests\TestCase;
 
-class TaggableTest extends TestCase
-{
+class TaggableTest extends TestCase {
 
-    public function setUp()
-    {
+    public function setUp() {
         parent::setUp();
         Artisan::call('migrate:refresh');
     }
 
     /**
-     * A test tags creation.
+     * Test tags creation.
      *
      * @return void
      */
-    public function testCreateTags()
-    {
-        $post = Post::create(['name' => 'demo', 'content' => 'lorem']);
-        echo $post;
+    public function testCreateTags() {
+        $post = factory(Post::class)->create();
         $post->saveTags('salut,chien,chat');
         $this->assertEquals(3, Tag::count());
     }
+
+    /**
+     * Test if a tag not recreated not so one created an article with an already existing tag
+     */
+    public function testReuseTags() {
+        $posts = factory(Post::class, 2)->create();
+        $post1 = $posts->first();
+        $post2 = $posts->last();
+        $post1->saveTags('salut,chien,chat');
+        $post2->saveTags('salut,chameau');
+        $this->assertEquals(4, Tag::count());
+    }
+
 }
