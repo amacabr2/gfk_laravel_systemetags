65f04a51c072b80613e554d3cf84021d68ebd457
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2017-05-01 16:22:36 +0200

    Plugin tokenfield

--- resources/views/application.blade.php
+++ resources/views/application.blade.php
@@ -11,6 +11,8 @@
         <link rel="icon" href="">
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
         <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+        <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
+        <link rel="stylesheet" href="{{ asset('css/bootstrap-tokenfield.min.css') }}">
         <title>@yield('title', 'Blog')</title>
     </head>
 
@@ -47,6 +49,9 @@
 
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+        <script src="{{ asset('js/bootstrap-tokenfield.min.js') }}"></script>
+        <script src="{{ asset('js/tokenfield.js') }}"></script>
 
     </body>
 
